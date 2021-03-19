3e8501ad26f1d82c55efa807c2be75d941232124
ANZ007 <arviandri.nz@gmail.com>
2021-03-20 02:05:12 +0700

    Adding last graded-assesment

---
+++ Course 6/Week 4/graded-assesment/.bash_history
@@ -0,0 +1,40 @@
+./health_check.py
+ls ~/
+sudo chmod +x ~/download_drive_file.sh
+./download_drive_file.sh 1LePo57dJcgzoK4uiI_48S01Etck7w_5f supplier-data.tar.gz
+tar xf ~/supplier-data.tar.gz
+ls ~/supplier-data
+cat ~/supplier-data/descriptions/007.txt
+nano ~/changeImage.py
+sudo chmod +x ~/changeImage.py
+./changeImage.py
+file ~/supplier-data/images/003.jpeg
+cat ~/example_upload.py
+sudo chmod +x ~/example_upload.py
+./example_upload.py
+nano ~/supplier_image_upload.py
+sudo chmod +x ~/supplier_image_upload.py
+./supplier_image_upload.py
+nano ~/run.py
+sudo chmod +x ~/run.py
+./run.py
+nano ~/reports.py
+sudo chmod +x ~/report_email.py
+sudo mv ~/report_email.py
+sudo mv ~/report_email.py ~/reports.py 
+ls
+sudo mv ~/reports.py ~/report_email.py 
+nano ~/reports.py
+nano ~/emails.py
+sudo chmod +x ~/report_email.py
+./report_email.py
+nano report_email.py 
+./report_email.py
+nano ~/health_check.py
+sudo chmod +x ~/health_check.py
+./health_check.py
+sudo apt install stress
+stress --cpu 8
+nano ~/health_check.py
+crontab -e
+exit
---
+++ Course 6/Week 4/graded-assesment/health_check.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python3
+
+import os
+import shutil
+import psutil
+import socket
+from emails import generate_error_report, send_email
+ 
+def check_cpu():
+    usage = psutil.cpu_percent(1)
+    return usage < 80
+ 
+def check_disk(disk):
+    du = shutil.disk_usage(disk)
+    free = du.free / du.total * 100
+    return free > 20
+ 
+def check_memory():
+    available_memory = psutil.virtual_memory().available/(1024*1024)
+    return available_memory > 500
+ 
+def check_localhost():
+    localhost = socket.gethostbyname('localhost')
+    return localhost == '127.0.0.1'
+ 
+to_be_checked = {
+    check_cpu(): "CPU usage is over 80%",
+    check_disk("/"): "Available disk space is less than 20%",
+    check_memory(): "Available memory is less than 500MB",
+    check_localhost(): "localhost cannot be resolved to 127.0.0.1"
+}
+
+error = False
+for action, message in to_be_checked.items():
+    if not action: 
+        error_message = message
+        error = True
+if error:
+    try:
+        sender = "automation@example.com"
+        receiver = "{}@example.com".format(os.environ.get('USER'))
+        subject = "Error - {}".format(error_message)
+        body = "Please check your system and resolve the issue as soon as possible"
+        message = generate_error_report(sender, receiver, subject, body)
+        send_email(message)
+        print('Detected Problem, check your email !')
+    except NameError:
+        pass
